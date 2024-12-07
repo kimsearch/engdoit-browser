e874d8a0f9eff95e78b0933adc5c74909debe2e8
hs770616 <hs770616@naver.com>
2024-12-08 02:39:23 +0900

    feat: change menifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,4 @@
-import type { Metadata } from "next";
 import "./globals.css";
-import Head from "next/head";
-
-export const metadata: Metadata = {
-  title: "engdoit",
-  description: "영단어 학습 앱",
-};
 
 export default function RootLayout({
   children,
@@ -14,10 +7,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#9aa7f9" />
-      </Head>
       <body>{children}</body>
     </html>
   );
